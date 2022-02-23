659ca65e067ffba696a4cf84e2f2ebae48880eff
Vahe Abazyan <va.abazyan@gmail.com>
2022-02-23 05:09:06 +0400

    no message

--- Restaurant/Controller/MenuTableViewController.swift
+++ Restaurant/Controller/MenuTableViewController.swift
@@ -7,20 +7,17 @@
 
 import UIKit
 
-class MenuTableViewController: UITableViewController {
+final class MenuTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return 0
     }
 }
